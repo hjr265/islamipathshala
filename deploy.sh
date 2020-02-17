528b9cb426d5cf8292fb0d1ba4778c57be143aec
Mahmud Ridwan <m@hjr265.me>
2020-02-18 00:19:41 +0600

    Integrate Workbox

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-hugo
+npm run build && \
 (cd public/ && s3cmd sync --delete-removed --acl-public --exclude sitemap.xml . s3://islamipathshala.com/)
---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const shell = require('gulp-shell');
+const workbox = require('workbox-build');
+
+var clean = shell.task('rm -r public/');
+
+var hugo = shell.task('hugo');
+
+function generateSW() {
+	return workbox.generateSW({
+		cacheId: 'pwapp',
+		globDirectory: './public',
+		globPatterns: [
+			'**/*.{css,js,png,svg,eot,ttf,woff,woff2,otf}'
+		],
+		swDest: './public/sw.js',
+		clientsClaim: true,
+		skipWaiting: true,
+		offlineGoogleAnalytics: true,
+		maximumFileSizeToCacheInBytes: 50*1024*1024,
+		runtimeCaching: [
+			{
+				urlPattern: /(?:\/)$/,
+				handler: "StaleWhileRevalidate",
+				options: {
+					cacheName: "html",
+					expiration: {
+						maxAgeSeconds: 60*60*24*7,
+					},
+				},
+			},
+		],
+	});
+};
+
+exports.default = gulp.series(clean, hugo, generateSW);
